808dd38504001e6a92910dfa3dd19be7abf6ffaa
Emmanuel M. Awoonor <coldfire@Emmanuels-MacBook-Pro.local>
2017-02-22 17:37:07 +0000

    work in progress: added the name of vendor or partner and replicated it

--- ppc-page.php
+++ ppc-page.php
@@ -25,6 +25,127 @@
         <img src="img/hub-logo-1.png" height="110" />
     </div>
     <div class="col-md-3 col-xs-6">
+        <p class="partner visible-xs">Name of Partner</p>
+        <p><i class="fa fa-check pc-check-mobile hidden-md hidden-lg" aria-hidden="true"></i><i class="fa fa-check pc-check hidden-xs" aria-hidden="true"></i> 100% free quotes</p>
+        <p><i class="fa fa-check pc-check-mobile hidden-md hidden-lg" aria-hidden="true"></i><i class="fa fa-check pc-check hidden-xs" aria-hidden="true"></i> No obligations</p>
+        <p><i class="fa fa-check pc-check-mobile hidden-md hidden-lg" aria-hidden="true"></i><i class="fa fa-check pc-check hidden-xs" aria-hidden="true"></i> Free gift <span class="hidden-xs"> on certain policies</span></p>
+    </div>
+    <div class="col-md-3 text-center hidden-xs">
+        <p class="btn-align">
+            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Range of benefits"><img src="img/key-features-1.png" width="50" /></a>
+            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Range of benefits"><img src="img/key-features-2.png" width="50" /></a>
+            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Range of benefits"><img src="img/key-features-3.png" width="50" /></a>
+            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Range of benefits"><img src="img/key-features-4.png" width="50" /></a>
+        </p>
+    </div>
+    <div class="col-md-2 text-center hidden-xs">
+        <div class="btn-align">
+            <a href="#" class="btn btn-md btn-warning pc-bold">APPLY NOW</a>
+        </div>
+        <div class="more-info">
+            <a href="#" class="more-info">more info...</a>
+        </div>
+    </div>
+
+    <div class="visible-xs">
+        <div class="col-xs-12">&nbsp;</div>
+        <div class="col-xs-6 text-center">
+            <a href="#" class="btn btn-md btn-default">Learn More <i class="fa fa-chevron-down" aria-hidden="true"></i></a>
+        </div>
+        <div class="col-xs-6">
+            <a href="#" class="btn btn-md btn-success">APPLY TODAY <i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+        </div>
+    </div>
+</div>
+<div class="row row-body">
+    <div class="col-md-1 text-center hidden-xs">
+        <p class="ordering"><span>1</span></p>
+    </div>
+    <div class="col-md-3 col-xs-6 text-center">
+        <img src="img/hub-logo-2.png" height="110" />
+    </div>
+    <div class="col-md-3 col-xs-6">
+        <p class="partner visible-xs">Name of Partner</p>
+        <p><i class="fa fa-check pc-check-mobile hidden-md hidden-lg" aria-hidden="true"></i><i class="fa fa-check pc-check hidden-xs" aria-hidden="true"></i> 100% free quotes</p>
+        <p><i class="fa fa-check pc-check-mobile hidden-md hidden-lg" aria-hidden="true"></i><i class="fa fa-check pc-check hidden-xs" aria-hidden="true"></i> No obligations</p>
+        <p><i class="fa fa-check pc-check-mobile hidden-md hidden-lg" aria-hidden="true"></i><i class="fa fa-check pc-check hidden-xs" aria-hidden="true"></i> Free gift <span class="hidden-xs"> on certain policies</span></p>
+    </div>
+    <div class="col-md-3 text-center hidden-xs">
+        <p class="btn-align">
+            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Range of benefits"><img src="img/key-features-1.png" width="50" /></a>
+            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Range of benefits"><img src="img/key-features-2.png" width="50" /></a>
+            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Range of benefits"><img src="img/key-features-3.png" width="50" /></a>
+            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Range of benefits"><img src="img/key-features-4.png" width="50" /></a>
+        </p>
+    </div>
+    <div class="col-md-2 text-center hidden-xs">
+        <div class="btn-align">
+            <a href="#" class="btn btn-md btn-warning pc-bold">APPLY NOW</a>
+        </div>
+        <div class="more-info">
+            <a href="#" class="more-info">more info...</a>
+        </div>
+    </div>
+
+    <div class="visible-xs">
+        <div class="col-xs-12">&nbsp;</div>
+        <div class="col-xs-6 text-center">
+            <a href="#" class="btn btn-md btn-default">Learn More <i class="fa fa-chevron-down" aria-hidden="true"></i></a>
+        </div>
+        <div class="col-xs-6">
+            <a href="#" class="btn btn-md btn-success">APPLY TODAY <i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+        </div>
+    </div>
+</div>
+<div class="row row-body">
+    <div class="col-md-1 text-center hidden-xs">
+        <p class="ordering"><span>1</span></p>
+    </div>
+    <div class="col-md-3 col-xs-6 text-center">
+        <img src="img/hub-logo-3.png" height="110" />
+    </div>
+    <div class="col-md-3 col-xs-6">
+        <p class="partner visible-xs">Name of Partner</p>
+        <p><i class="fa fa-check pc-check-mobile hidden-md hidden-lg" aria-hidden="true"></i><i class="fa fa-check pc-check hidden-xs" aria-hidden="true"></i> 100% free quotes</p>
+        <p><i class="fa fa-check pc-check-mobile hidden-md hidden-lg" aria-hidden="true"></i><i class="fa fa-check pc-check hidden-xs" aria-hidden="true"></i> No obligations</p>
+        <p><i class="fa fa-check pc-check-mobile hidden-md hidden-lg" aria-hidden="true"></i><i class="fa fa-check pc-check hidden-xs" aria-hidden="true"></i> Free gift <span class="hidden-xs"> on certain policies</span></p>
+    </div>
+    <div class="col-md-3 text-center hidden-xs">
+        <p class="btn-align">
+            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Range of benefits"><img src="img/key-features-1.png" width="50" /></a>
+            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Range of benefits"><img src="img/key-features-2.png" width="50" /></a>
+            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Range of benefits"><img src="img/key-features-3.png" width="50" /></a>
+            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Range of benefits"><img src="img/key-features-4.png" width="50" /></a>
+        </p>
+    </div>
+    <div class="col-md-2 text-center hidden-xs">
+        <div class="btn-align">
+            <a href="#" class="btn btn-md btn-warning pc-bold">APPLY NOW</a>
+        </div>
+        <div class="more-info">
+            <a href="#" class="more-info">more info...</a>
+        </div>
+    </div>
+
+    <div class="visible-xs">
+        <div class="col-xs-12">&nbsp;</div>
+        <div class="col-xs-6 text-center">
+            <a href="#" class="btn btn-md btn-default">Learn More <i class="fa fa-chevron-down" aria-hidden="true"></i></a>
+        </div>
+        <div class="col-xs-6">
+            <a href="#" class="btn btn-md btn-success">APPLY TODAY <i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+        </div>
+    </div>
+</div>
+<div class="row row-body">
+    <div class="col-md-1 text-center hidden-xs">
+        <p class="ordering"><span>1</span></p>
+    </div>
+    <div class="col-md-3 col-xs-6 text-center">
+        <img src="img/hub-logo-4.png" height="110" />
+    </div>
+    <div class="col-md-3 col-xs-6">
+        <p class="partner visible-xs">Name of Partner</p>
         <p><i class="fa fa-check pc-check-mobile hidden-md hidden-lg" aria-hidden="true"></i><i class="fa fa-check pc-check hidden-xs" aria-hidden="true"></i> 100% free quotes</p>
         <p><i class="fa fa-check pc-check-mobile hidden-md hidden-lg" aria-hidden="true"></i><i class="fa fa-check pc-check hidden-xs" aria-hidden="true"></i> No obligations</p>
         <p><i class="fa fa-check pc-check-mobile hidden-md hidden-lg" aria-hidden="true"></i><i class="fa fa-check pc-check hidden-xs" aria-hidden="true"></i> Free gift <span class="hidden-xs"> on certain policies</span></p>
